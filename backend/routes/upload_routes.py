2a1d6a194bce6b0905b563e281535e5ead5ed17b
Arashi-vision <karanworkhub@gmail.com>
2025-11-25 08:02:59 +0530

    Update upload_routes.py

--- backend/routes/upload_routes.py
+++ backend/routes/upload_routes.py
@@ -1,4 +1,4 @@
-```python
+
 from fastapi import APIRouter, UploadFile
 
 upload_router = APIRouter()
@@ -6,4 +6,4 @@ upload_router = APIRouter()
 @upload_router.post("/file")
 def upload(file: UploadFile):
     return {"filename": file.filename}
-```
+
