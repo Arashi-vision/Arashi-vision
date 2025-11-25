41cab5ec55aeafefb3a9ddd8a731f070262d3200
Arashi-vision <karanworkhub@gmail.com>
2025-11-25 08:03:24 +0530

    Update voice_routes.py

--- backend/routes/voice_routes.py
+++ backend/routes/voice_routes.py
@@ -1,5 +1,4 @@
 
-```python
 from fastapi import APIRouter
 
 voice_router = APIRouter()
@@ -7,4 +6,4 @@ voice_router = APIRouter()
 @voice_router.post("/stt")
 def stt():
     return {"stt": True}
-```
+
